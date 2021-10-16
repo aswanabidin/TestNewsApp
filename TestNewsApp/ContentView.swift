553ef8fea8e0110b16cc81bb275d0a3354ab0ef5
Aswan Abidin <aswan.abidin@kitabisa.com>
2021-10-16 11:14:31 +0800

    Initial Commit

---
+++ TestNewsApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestNewsApp
+//
+//  Created by Aswan Abidin on 16/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestNewsApp/TestNewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestNewsAppApp.swift
+//  TestNewsApp
+//
+//  Created by Aswan Abidin on 16/10/21.
+//
+
+import SwiftUI
+
+@main
+struct TestNewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
